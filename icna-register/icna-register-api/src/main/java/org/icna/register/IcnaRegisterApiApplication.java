3e3e39a2c6a807d467f0e02bc28bd220fa27ed34
sheraz <sheraz@mbp16.com>
2024-06-28 22:04:57 -0400

    ICNA Register

---
+++ icna-register/icna-register-api/src/main/java/org/icna/register/IcnaRegisterApiApplication.java
@@ -0,0 +1,13 @@
+package org.icna.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcnaRegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcnaRegisterApiApplication.class, args);
+	}
+
+}
