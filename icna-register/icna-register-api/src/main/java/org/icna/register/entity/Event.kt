e169d443fd1945bcff08547a695859feb138d7d8
sheraz <sheraz@mbp16.com>
2024-07-03 03:45:43 -0400

    ICNA Register - Event DB

--- icna-register/icna-register-api/src/main/java/org/icna/register/entity/Event.kt
+++ icna-register/icna-register-api/src/main/java/org/icna/register/entity/Event.kt
@@ -1,4 +1,16 @@
 package org.icna.register.entity
 
-class Event {
-}
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+data class Event(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int?,
+    @Column(nullable = false)
+    val name: String
+)
