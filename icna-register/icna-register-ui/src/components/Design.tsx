ed3faab8c2c99afd787508fa6df1bf59b0472ffa
sheraz <sheraz@mbp16.com>
2024-07-21 11:21:00 -0400

    ICNA Register - Design common form styles

--- icna-register/icna-register-ui/src/components/Design.tsx
+++ icna-register/icna-register-ui/src/components/Design.tsx
@@ -1,5 +1,5 @@
 import React, {CSSProperties} from "react";
-import formStyles from "./FormStyles.module.scss";
+import "./FormStyles.module.scss";
 
 interface Props {
 }
@@ -12,9 +12,37 @@ const getVars = () => {
 
 export const Design: React.FC<Props> = () => {
     return (
-        <div>
-            <div>
-                <input className={formStyles.testClass}/>
+        <div style={{margin: "20px"}}>
+            <div style={{margin: "10px"}}>
+                <input type="button" value="Click me"/>
+            </div>
+            <div style={{margin: "10px"}}>
+                <label htmlFor="inputCheckbox">Input checkbox</label>
+                <input id="inputCheckbox" type="checkbox"/>
+            </div>
+            <div style={{margin: "10px"}}>
+                <label htmlFor="inputRadio1">Input Radio 1</label>
+                <input id="inputRadio1" type="radio" name="inputRadio"/>
+                <label htmlFor="inputRadio2">Input Radio 2</label>
+                <input id="inputRadio2" type="radio" name="inputRadio"/>
+            </div>
+            <div style={{margin: "10px"}}>
+                <label htmlFor="inputText">Input Text</label>
+                <input id="inputText" type="text" placeholder="input text"/>
+            </div>
+            <div style={{margin: "10px"}}>
+                <label htmlFor="inputDate">Input Date</label>
+                <input id="inputDate" type="date"/>
+            </div>
+            <div style={{margin: "10px"}}>
+                <label htmlFor="inputDateTimeLocal">Input Date Time Local</label>
+                <input id="inputDateTimeLocal" type="datetime-local"/>
+            </div>
+            <div style={{margin: "10px"}}>
+                <label htmlFor="inputDateTimeLocal">Input Date Time Local</label>
+                <br/>
+                <textarea name="" id="" cols={30} rows={10}>
+                </textarea>
             </div>
         </div>
     );
