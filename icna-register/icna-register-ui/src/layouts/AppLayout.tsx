d8e6dfdf066527d0b38011c730ad9b452b315825
sheraz <sheraz@mbp16.com>
2024-07-23 19:21:15 -0400

    ICNA Register - Design app layout

--- icna-register/icna-register-ui/src/layouts/AppLayout.tsx
+++ icna-register/icna-register-ui/src/layouts/AppLayout.tsx
@@ -1,11 +1,14 @@
 import AppNav from "../navigations/AppNav";
 import {Outlet} from "react-router-dom";
+import styles from "./AppLayout.module.scss";
 
 export default function AppLayout() {
     return(
-        <div>
-            <AppNav />
-            <Outlet />
+        <div className={styles.appBackground}>
+            <div className={styles.appContainer}>
+                <AppNav />
+                <Outlet />
+            </div>
         </div>
     );
 }
