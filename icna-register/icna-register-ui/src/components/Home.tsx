80ca426eaa2a4f882b48e78d501853dbe45fda31
sheraz <sheraz@mbp16.com>
2024-08-06 22:02:45 -0400

    ICNA Register - Auth UI

--- icna-register/icna-register-ui/src/components/Home.tsx
+++ icna-register/icna-register-ui/src/components/Home.tsx
@@ -69,11 +69,11 @@ export const Home: React.FC<Props> = () => {
                         // width="100px"
                         // height="100px"
                         style={{
-                            width: "100px", height: "100px",
-                            color: "red", backgroundColor: "purple"
+                            width: "400px", height: "400px",
+                            backgroundColor: "purple",
+                            stroke: "#ff0000",
+                            fill: "#00ff00"
                         }}
-                        stroke="#ff0000"
-                        fill="#00ff00"
                         viewBox="0 0 100 100">
                         <path d="M37.34 99.381l37.224-49.585L36.877.618 25.435 10.744l30.009 39.158-29.639 39.481z" />
                     </svg>
