aada8cd6627dc410ff4ac15bab6176b36189e5b3
sheraz <sheraz@mbp16.com>
2024-08-11 16:43:26 -0400

    ICNA Register - Auth UI

--- icna-register/icna-register-ui/src/images/IconArrowRight.tsx
+++ icna-register/icna-register-ui/src/images/IconArrowRight.tsx
@@ -5,23 +5,20 @@ interface Props {
     height?: string;
     width?: string;
     fill?: string;
-    className?: string;
 }
 
 export const IconArrowRight: React.FC<Props> = (
     {
         height,
         width,
-        fill,
-        className
+        fill
     }) => {
     return (
         <Icon
             height={height}
             width={width}
             fill={fill}
-            className={className}
             pathD="M37.34 99.381l37.224-49.585L36.877.618 25.435 10.744l30.009 39.158-29.639 39.481z"
         />
     );
-}
+};
