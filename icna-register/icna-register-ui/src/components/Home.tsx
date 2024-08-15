728c208afca8676f88dd3116fe2065face872ace
sheraz <sheraz@mbp16.com>
2024-08-15 00:26:51 -0400

    ICNA Register - Auth UI

--- icna-register/icna-register-ui/src/components/Home.tsx
+++ icna-register/icna-register-ui/src/components/Home.tsx
@@ -1,11 +1,10 @@
 import React, {useContext, useEffect, useState} from "react";
 import {EventDto, defaultEventDto, EventProgramDto} from "../service/service-types";
 import {registerApis} from "../service/api/ApiRegister";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import {AppContext} from "../store/context";
 import {createLoadingActionHide, createLoadingActionShow} from "./Loading";
 import styles from "./Home.module.scss"
-import {Icon} from "../images/Icon";
 import {IconArrowRight} from "../images/IconArrowRight";
 
 interface Props {
@@ -48,30 +47,32 @@ export const Home: React.FC<Props> = () => {
             <div>
                 <h1>{event.eventName}</h1>
             </div>
-
             <div className={styles.eventProgramsLoginContainer}>
-                <div>Register </div>
                 <div className={styles.eventProgramsContainer}>
                     <div><h2>Event Programs</h2></div>
                     {eventProgramDtoArray.map(ep => (
                         <div key={ep.id}>{ep.programName}</div>
                     ))}
+
+                    <div style={{marginTop: "32px"}}>
+                        <Link to={`/event/${eventId}/register/new`}>Register <IconArrowRight/></Link>
+                    </div>
                 </div>
                 <div className={styles.loginContainer}>
                     <div className={styles.loginBox}>
-                        Login
-                        <input placeholder="Email" />
+                        <div style={{marginBottom: "20px"}}>
+                            <label htmlFor="">Login</label>
+                        </div>
+                        <input placeholder="Email" style={{width: "290px"}}/>
+                        <a style={{justifyContent: "right", marginTop: "20px"}} href="#">
+                            Login with password <IconArrowRight />
+                        </a>
+                        <a style={{justifyContent: "right", marginTop: "20px"}} href="#">
+                            Login email code <IconArrowRight />
+                        </a>
                     </div>
                 </div>
             </div>
-
-            <div>
-                <div>
-                    <IconArrowRight />
-
-                </div>
-            </div>
-
         </div>
     );
 }
