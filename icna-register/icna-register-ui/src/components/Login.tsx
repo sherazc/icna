d7dbdbe71e01a9bf5db07b4cb1234aa7a7e2a01f
sheraz <sheraz@mbp16.com>
2024-08-30 07:50:37 -0400

    ICNA Register - Auth UI

--- icna-register/icna-register-ui/src/components/Login.tsx
+++ icna-register/icna-register-ui/src/components/Login.tsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Login.module.scss";
 import {IconArrowRight} from "../images/IconArrowRight";
+import {useParams} from "react-router-dom";
+import {touchString} from "../service/utilities";
 
 interface Props {
 }
 
+type LoginRequest = {
+    eventId: string,
+    email: string,
+    userPassword?: string
+    oneTimeUseCode?: string;
+}
+
 export const Login: React.FC<Props> = () => {
+
+    const {eventId} = useParams();
+
+    const [loginRequest, setLoginRequest] = useState<LoginRequest>({
+        email: "", eventId: touchString(eventId)
+    });
+
+    const onChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = event.target;
+        setLoginRequest(prevData => ({ ...prevData, [id]: value }));
+    };
+
     return (
         <div className={styles.loginContainer}>
             <div className={styles.loginBox}>
                 <div style={{marginBottom: "20px"}}>
                     <label htmlFor="">Login</label>
                 </div>
-                <input placeholder="Email" style={{width: "290px"}}/>
+                <input
+                    id="email"
+                    value={loginRequest.email}
+                    onChange={onChangeText}
+                    placeholder="Email"
+                    style={{width: "290px"}}/>
                 <a style={{justifyContent: "right", marginTop: "20px"}} href="#">
                     Login with password <IconArrowRight/>
                 </a>
