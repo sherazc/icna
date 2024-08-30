bbce2d683d77a7be3c9ee87dd58b1dc922fffeac
sheraz <sheraz@mbp16.com>
2024-08-30 08:33:19 -0400

    ICNA Register - Auth UI

--- icna-register/icna-register-ui/src/components/Login.tsx
+++ icna-register/icna-register-ui/src/components/Login.tsx
@@ -14,6 +14,11 @@ type LoginRequest = {
     oneTimeUseCode?: string;
 }
 
+enum LoginState {
+    SHOW_EMAIL, SHOW_PASSWORD, LOADING, SHOW_SUBMIT,
+    SHOW_OTU_CODE, LOGIN_SUCCESS, LOGIN_FAILURE
+}
+
 export const Login: React.FC<Props> = () => {
 
     const {eventId} = useParams();
@@ -21,6 +26,8 @@ export const Login: React.FC<Props> = () => {
     const [loginRequest, setLoginRequest] = useState<LoginRequest>({
         email: "", eventId: touchString(eventId)
     });
+
+    const [loginState, setLoginState] = useState<LoginState>(LoginState.SHOW_EMAIL);
 
     const onChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { id, value } = event.target;
@@ -33,18 +40,31 @@ export const Login: React.FC<Props> = () => {
                 <div style={{marginBottom: "20px"}}>
                     <label htmlFor="">Login</label>
                 </div>
-                <input
+
+                {loginState === LoginState.SHOW_EMAIL && (<input
                     id="email"
                     value={loginRequest.email}
                     onChange={onChangeText}
                     placeholder="Email"
-                    style={{width: "290px"}}/>
-                <a style={{justifyContent: "right", marginTop: "20px"}} href="#">
-                    Login with password <IconArrowRight/>
-                </a>
-                <a style={{justifyContent: "right", marginTop: "20px"}} href="#">
-                    Login email code <IconArrowRight/>
-                </a>
+                    style={{width: "290px"}}/>)}
+
+                {loginState === LoginState.SHOW_PASSWORD && (<input
+                    id="userPassword"
+                    value={loginRequest.userPassword}
+                    onChange={onChangeText}
+                    placeholder="Enter Password"
+                    style={{width: "290px"}}/>)}
+
+                {loginState === LoginState.SHOW_EMAIL && (<>
+                    <a onClick={() => setLoginState(LoginState.SHOW_PASSWORD)}
+                       style={{justifyContent: "right", marginTop: "20px"}} href="#">
+                        Login with password <IconArrowRight/>
+                    </a>
+                    <a style={{justifyContent: "right", marginTop: "20px"}} href="#">
+                        Login email code <IconArrowRight/>
+                    </a>
+                </>)}
+
             </div>
         </div>
     );
