6e29a3047a88d3fc249f081539ee3e48cc82d3b0
sheraz <sheraz@mbp16.com>
2024-09-15 17:19:03 -0400

    ICNA Register - Save Registration

--- icna-register/icna-register-api/src/test/java/org/icna/register/service/UserProfileServiceTest.kt
+++ icna-register/icna-register-api/src/test/java/org/icna/register/service/UserProfileServiceTest.kt
@@ -1,26 +1,55 @@
 package org.icna.register.service
 
+import org.icna.register.dto.UserProfileDto
 import org.icna.register.repository.UserProfileRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.anyLong
+import org.mockito.Mockito.anyString
 
 import org.mockito.Mockito.mock
+import java.util.Optional
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class UserProfileServiceTest {
 
     lateinit var userProfileRepository: UserProfileRepository
 
+    lateinit var underTest: UserProfileService
+
     @BeforeEach
     fun setUp() {
         userProfileRepository = mock(UserProfileRepository::class.java)
+        underTest = UserProfileService(userProfileRepository)
+    }
+
+    @Test
+    fun findByEventIdAndUserEmailNoPassword_noEmail() {
+        val user = underTest.findByEventIdAndUserEmailNoPassword(1, "")
+        assertTrue(user.isEmpty)
+    }
+
+    @Test
+    fun findByEventIdAndUserEmailNoPassword_notFound() {
+        Mockito.`when`(userProfileRepository.findByEventAndEmail(anyLong(), anyString()))
+            .thenReturn(Optional.empty())
+        val user = underTest.findByEventIdAndUserEmailNoPassword(1, "abc")
+        assertTrue(user.isEmpty)
     }
 
     @Test
     fun findByEventIdAndUserEmailNoPassword() {
-
-
-
-
+        val expected = UserProfileDto(100, "", null, 1)
+        Mockito.`when`(userProfileRepository.findByEventAndEmail(anyLong(), anyString()))
+            .thenReturn(Optional.of(expected))
+        val user = underTest.findByEventIdAndUserEmailNoPassword(1, "abc")
+        assertTrue(user.isPresent)
+        assertEquals(expected.id, user.get().id)
+        assertEquals(expected.email, user.get().email)
+        assertEquals(expected.eventId, user.get().eventId)
+        assertEquals(expected.userPassword, user.get().userPassword)
     }
 
     @Test
