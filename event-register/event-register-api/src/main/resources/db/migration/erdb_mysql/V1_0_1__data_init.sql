aa8d18f999499a9f27df5709d75542c8a3dbb8b6
sheraz <sheraz@mbp16.com>
2024-10-11 09:08:10 -0400

    Refactor

--- event-register/event-register-api/src/main/resources/db/migration/erdb_mysql/V1_0_1__data_init.sql
+++ event-register/event-register-api/src/main/resources/db/migration/erdb_mysql/V1_0_1__data_init.sql
@@ -22,16 +22,16 @@ values (420, 'BASIC_USER');
 
 
 insert into user_profile(id, email, user_password, event_id)
-values (700, 'admin@event.org', '{noop}abc', 10);
+values (700, 'admin@event.com', '{noop}abc', 10);
 
 insert into user_profile(id, email, user_password, event_id)
-values (710, 'assitant@event.org', '{noop}abc', 10);
+values (710, 'assitant@event.com', '{noop}abc', 10);
 
 insert into user_profile(id, email, user_password, event_id)
-values (720, 'basic_user_sheraz@event.org', '{noop}abc', 10);
+values (720, 'basic_user_sheraz@event.com', '{noop}abc', 10);
 
 insert into user_profile(id, email, user_password, event_id)
-values (730, 'basic_user_baker@event.org', '{noop}abc', 10);
+values (730, 'basic_user_baker@event.com', '{noop}abc', 10);
 
 
 insert into m2m_user_profile_user_role(user_profile_id, user_role_id)
