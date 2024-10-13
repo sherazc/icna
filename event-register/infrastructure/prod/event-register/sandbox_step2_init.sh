145884c37bcf326d054604332e2eddd1886a7e7c
sheraz <sheraz@mbp16.com>
2024-10-13 08:42:48 -0400

    Setting up production environment

--- event-register/infrastructure/prod/event-register/sandbox_step2_init.sh
+++ event-register/infrastructure/prod/event-register/sandbox_step2_init.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+systemctl disable event_register.service
+systemctl stop event_register
+
 rm -rf /opt/sheraz-infra/event-register
 mkdir -p /opt/sheraz-infra/event-register
 
-mv /home/sheraz/event-register/app /opt/sheraz-infra/event-register
+mv /home/sheraz/dev/event-register/app /opt/sheraz-infra/event-register
 
-# setup service
+rm /etc/systemd/system/event_register.service
+mv /home/sheraz/dev/event-register/event_register.service /etc/systemd/system/
 
-echo ====[ Successfully installed event-register. ]====
-echo ====[ Now restart. sudo systemctl restart event-register. ]====
+systemctl daemon-reload
+systemctl start event_register
+systemctl enable event_register.service
+
+echo ====[ Successfully installed started event-register service. ]====
+echo ====[ Check status. sudo systemctl status event_register ]====
--- event-register/infrastructure/prod/sandbox_step1_setup.sh
+++ event-register/infrastructure/prod/sandbox_step1_setup.sh
@@ -11,12 +11,12 @@ cd infrastructure/prod
 # copy infra
 scp -r -i ~/.ssh/id_rsa \
   ./event-register \
-  sheraz@10.0.0.196:/home/sheraz/
+  sheraz@10.0.0.196:/home/sheraz/dev/
 
 # Copy app
 scp -r -i ~/.ssh/id_rsa \
   ../../event-register-api/target/event-register.jar \
-  sheraz@10.0.0.196:/home/sheraz/event-register/app
+  sheraz@10.0.0.196:/home/sheraz/dev/event-register/app
 
 echo ====[ Successfully cleaned, built and deployed on sheraz-infra. ]====
 echo ====[ Now run /home/sheraz/event-register/sandbox_step2_init.sh. ]====
