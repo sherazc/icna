7dbff6ecbdb13be005652c27b10a8931ac1168f6
sheraz <sheraz@mbp16.com>
2024-10-24 08:50:23 -0400

    Manage Event

--- event-register/event-register-ui/src/components/ManageEvent.tsx
+++ event-register/event-register-ui/src/components/ManageEvent.tsx
@@ -1,14 +1,24 @@
-import React from "react";
-import {useNavigate, useParams} from "react-router-dom";
+import React, {useContext} from "react";
+import {Navigate, useNavigate, useParams} from "react-router-dom";
+import {AppContext} from "../store/context";
 
 export const ManageEvent = () => {
-    const navigate = useNavigate();
     const {eventId} = useParams();
+    const navigate = useNavigate();
+    const [{authUserToken}, dispatch] = useContext(AppContext);
     const cancelLink = eventId ? `/event/${eventId}` : "/";
+    const authenticated = authUserToken.userProfileId > 0;
+    const adminUser = authUserToken.roles.includes("ADMIN");
+
+    if (authenticated && !adminUser) {
+        return <Navigate to={`/event/${eventId}`} />
+    }
 
     return (
         <div>
             <h1>Event</h1>
+            {adminUser ?  "I am admin." : "I am not admin."}
+            {authenticated ?  "I am authenticated." : "I am not authenticated."}
             <div>
                 <button onClick={() => navigate(cancelLink)}>Cancel</button>
             </div>
