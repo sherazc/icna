e7b140faad5f3201f8fdb4ccb89a6cff12f2f477
sheraz <sheraz@mbp16.com>
2025-02-16 09:49:37 -0500

    Manage Event - Strip

---
+++ event-register/event-register-api/src/main/java/com/sc/event/service/payment/StripService.kt
@@ -0,0 +1,58 @@
+package com.sc.event.service.payment
+
+import com.stripe.Stripe
+import com.stripe.model.checkout.Session
+import com.stripe.param.checkout.SessionCreateParams
+import org.springframework.beans.factory.annotation.Value
+
+class StripService(
+    @Value("com.sc.event.payment.stripe.secret")
+    private val secretKey: String) {
+
+    fun createSessionUrl(): String {
+        Stripe.apiKey = secretKey
+
+        val parameters = buildParameters()
+        val session = Session.create(parameters)
+
+        return session.url
+    }
+
+    private fun buildParameters(): SessionCreateParams {
+        val lineItem = buildLineItem()
+
+        val sessionParameters = SessionCreateParams.builder()
+            .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+            .setMode(SessionCreateParams.Mode.PAYMENT)
+            .setSuccessUrl("https://mail.google.com/")
+            .setCancelUrl("https://www.cnn.com/")
+            .addLineItem(lineItem) // Could be called multiple times
+            .build()
+        return sessionParameters
+    }
+
+    private fun buildLineItem(): SessionCreateParams.LineItem {
+        val priceData = buildPriceData()
+
+        return SessionCreateParams.LineItem.builder()
+            .setQuantity(1)
+            .setPriceData(priceData)
+            .build()
+    }
+
+    private fun buildPriceData(): SessionCreateParams.LineItem.PriceData {
+        val productData = buildProductData()
+
+        return SessionCreateParams.LineItem.PriceData.builder()
+            .setCurrency("usd")
+            .setUnitAmount((19.99 * 100).toLong()) // Convert to cents
+            .setProductData(productData)
+            .build()
+    }
+
+    private fun buildProductData(): SessionCreateParams.LineItem.PriceData.ProductData {
+        return SessionCreateParams.LineItem.PriceData.ProductData.builder()
+            .setName("Event Registration")
+            .build()
+    }
+}
