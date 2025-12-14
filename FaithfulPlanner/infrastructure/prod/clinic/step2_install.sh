2d87b3af303691fc2cfcbfacfad494419160994d
sheraz@mbp16 <sheraz@bitsegment.com>
2025-12-14 17:20:37 -0500

    build script

--- FaithfulPlanner/infrastructure/prod/clinic/step2_install.sh
+++ FaithfulPlanner/infrastructure/prod/clinic/step2_install.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 
-systemctl disable event_register.service
-systemctl stop event_register
+sudo systemctl start docker
 
-rm -rf /opt/sheraz-infra/event-register
-mkdir -p /opt/sheraz-infra/event-register
-
-mv /home/sheraz/dev/event-register/app /opt/sheraz-infra/event-register
-
-rm /etc/systemd/system/event_register.service
-mv /home/sheraz/dev/event-register/event_register.service /etc/systemd/system/
-
-systemctl daemon-reload
-systemctl start event_register
-systemctl enable event_register.service
 
 echo ====[ Successfully installed started event-register service. ]====
 echo ====[ Check status. sudo systemctl status event_register ]====
--- FaithfulPlanner/infrastructure/prod/step1_build_deploy.sh
+++ FaithfulPlanner/infrastructure/prod/step1_build_deploy.sh
@@ -5,7 +5,7 @@ source env_pi
 
 cd ..
 # ./clean.sh
-# ./build.sh
+./build.sh
 
 cd $app_infra_source_path
 
@@ -16,13 +16,13 @@ ssh -i ~/.ssh/id_rsa \
 
 # Copy infra
 scp -r -i ~/.ssh/id_rsa \
-  ./clinic \
+  ./$app_name \
   $app_server_user@$app_server_ip:$app_server_path
 
-## Copy app
-#scp -r -i ~/.ssh/id_rsa \
-#  ../../event-register-api/target/event-register.jar \
-#  sheraz@10.0.0.196:/home/sheraz/dev/event-register/app
-#
+# Copy app
+scp -r -i ~/.ssh/id_rsa \
+  ../../api/build/libs/api-1.0.jar \
+  $app_server_user@$app_server_ip:$app_server_path/$app_name/$app_name.jar
+
 #echo ====[ Successfully cleaned, built and deployed on sheraz-infra. ]====
 #echo ====[ Now run: sudo /home/sheraz/dev/event-register/step2_install.sh. ]====
