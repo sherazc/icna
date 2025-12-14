bee3f6e239d1e326ffc87ae53e338cddce62d6d2
sheraz@mbp16 <sheraz@bitsegment.com>
2025-12-14 13:15:17 -0500

    build script

--- FaithfulPlanner/infrastructure/prod/step1_build_deploy.sh
+++ FaithfulPlanner/infrastructure/prod/step1_build_deploy.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 # Build
+cd ..
+source env_pi
 
-cd ../../
- ./clean.sh
- ./build.sh
+cd ..
+# ./clean.sh
+# ./build.sh
 
-cd infrastructure/prod
+cd $app_infra_source_path
 
-# Copy infrastructure
-# copy infra
+
+ssh -i ~/.ssh/id_rsa \
+  "$app_server_user@$app_server_ip" \
+  "mkdir -p '$app_server_path'"
+
+# Copy infra
 scp -r -i ~/.ssh/id_rsa \
-  ./event-register \
-  sheraz@10.0.0.196:/home/sheraz/dev/
+  ./clinic \
+  $app_server_user@$app_server_ip:$app_server_path
 
-# Copy app
-scp -r -i ~/.ssh/id_rsa \
-  ../../event-register-api/target/event-register.jar \
-  sheraz@10.0.0.196:/home/sheraz/dev/event-register/app
-
-echo ====[ Successfully cleaned, built and deployed on sheraz-infra. ]====
-echo ====[ Now run: sudo /home/sheraz/dev/event-register/step2_install.sh. ]====
+## Copy app
+#scp -r -i ~/.ssh/id_rsa \
+#  ../../event-register-api/target/event-register.jar \
+#  sheraz@10.0.0.196:/home/sheraz/dev/event-register/app
+#
+#echo ====[ Successfully cleaned, built and deployed on sheraz-infra. ]====
+#echo ====[ Now run: sudo /home/sheraz/dev/event-register/step2_install.sh. ]====
