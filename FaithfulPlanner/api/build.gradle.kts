356b009a5d55d707df113cce4745096801d8ebcb
sheraz@mbp16 <sheraz@bitsegment.com>
2025-12-21 09:54:29 -0500

    Flyway

--- FaithfulPlanner/api/build.gradle.kts
+++ FaithfulPlanner/api/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("jvm") version "2.3.0"  // Add Kotlin JVM plugin
 	id("org.springframework.boot") version "4.0.0"
 	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("plugin.spring") version "2.2.0"
 }
 
 group = "com.sc.clinic"
@@ -24,18 +25,16 @@ repositories {
 	mavenCentral()
 }
 
-ext {
-	set("flywayVersion", "10.21.0")
-}
-
 dependencies {
 	implementation("org.springframework.boot:spring-boot-h2console")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	implementation("org.flywaydb:flyway-core:10.21.0")
-	implementation("org.flywaydb:flyway-database-postgresql:10.21.0")
+//	implementation("org.flywaydb:flyway-core:10.10.0")
+//	implementation("org.flywaydb:flyway-database-postgresql:10.10.0")
+	implementation("org.springframework.boot:spring-boot-starter-flyway")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")  // Add Kotlin standard library
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
