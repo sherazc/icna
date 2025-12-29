f9d5c74a58b5d3c518ac81ee7d9b2c11ead9756e
sheraz@mbp16 <sheraz@bitsegment.com>
2025-12-29 08:27:25 -0600

    Register Company and admin

--- FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/exception/ScGlobalExceptionHandler.kt
+++ FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/exception/ScGlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.sc.clinic.exception
 
+import com.sc.clinic.dto.ScErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
@@ -17,24 +18,24 @@ class ScGlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleException(exception: MethodArgumentNotValidException): ResponseEntity<MyErrorResponse?> {
-        val bindingResult = exception.getBindingResult()
+    fun handleException(exception: MethodArgumentNotValidException): ResponseEntity<ScErrorResponse> {
+        val bindingResult = exception.bindingResult
 
-        val errorMessage = bindingResult.getFieldErrors().stream()
-            .map<String?> { f: FieldError? -> f!!.getField() + ":" + f.getDefaultMessage() }
+        val errorMessage = bindingResult.fieldErrors.stream()
+            .map<String?> { f: FieldError? -> f!!.field + ":" + f.defaultMessage }
             .collect(Collectors.joining(","))
 
-        val fields = bindingResult.getFieldErrors().stream()
-            .map<String?> { obj: FieldError? -> obj!!.getField() }
+        val fields = bindingResult.fieldErrors.stream()
+            .map<String?> { obj: FieldError? -> obj!!.field }
             .collect(Collectors.joining("."))
 
-        val errorResponse: MyErrorResponse = MyErrorResponse(
+        val errorResponse: ScErrorResponse = ScErrorResponse(
             String.format("Invalid Request: %s", errorMessage),
             fields,
             "400"
         )
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body<MyErrorResponse?>(errorResponse)
+            .body<ScErrorResponse>(errorResponse)
     }
 }
