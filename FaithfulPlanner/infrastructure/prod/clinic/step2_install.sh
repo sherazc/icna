76cc276a38ca0bedb5f7498f5c85a72592710d2f
sheraz@mbp16 <sheraz@bitsegment.com>
2026-01-01 10:23:41 -0500

    Infrastructure

--- FaithfulPlanner/infrastructure/prod/clinic/step2_install.sh
+++ FaithfulPlanner/infrastructure/prod/clinic/step2_install.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+ln -s ./infrastructure/prod/clinic/.env .env
+
 sudo systemctl start docker
+
+echo Building clinic image
+
+echo Building clinic image
 docker stop clinic || true
 docker rm clinic || true
 docker rmi clinic -f || true
 docker build . -t clinic
 
+echo Running clinic container
 docker run -it \
   --name clinic \
   -p 8080:8080 \
