a17a8e4b7273148f9cd286be780f604dbc6d5627
sheraz@mbp16 <sheraz@bitsegment.com>
2025-12-31 23:19:35 -0500

    Register Company and admin api

--- FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/service/RegistrationSaveService.kt
+++ FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/service/RegistrationSaveService.kt
@@ -3,7 +3,6 @@ package com.sc.clinic.service
 import com.sc.clinic.dto.CompanyDto
 import com.sc.clinic.dto.RegistrationDto
 import com.sc.clinic.dto.UserProfileDto
-import com.sc.clinic.exception.ScBadRequestException
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
@@ -23,7 +22,9 @@ class RegistrationSaveService(
         val userProfileEntity = userProfileService.saveRegistrationAdmin(companyEntity, registrationDto.adminUser)
 
         // Build response
-        return RegistrationDto(CompanyDto(companyEntity), UserProfileDto(userProfileEntity))
+        val adminUser = UserProfileDto(userProfileEntity)
+        adminUser.usersPassword = null
+        return RegistrationDto(CompanyDto(companyEntity), adminUser)
     }
 
     private fun validate(registrationDto: RegistrationDto) {
