a50e5204b84d5c0fb4ca904b59bc16a87e94e19e
sheraz@mbp16 <sheraz@bitsegment.com>
2026-01-25 18:39:11 -0500

    Employee group api

--- FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/controller/EmployeeGroupController.kt
+++ FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/controller/EmployeeGroupController.kt
@@ -1,22 +1,17 @@
 package com.sc.clinic.controller
 
-import com.sc.clinic.dto.RegistrationDto
 import com.sc.clinic.service.EmployeeGroupService
-import com.sc.clinic.service.RegistrationSaveService
-import jakarta.validation.Valid
-import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/api/company/{companyId}/employee-group")
 @RestController
 class EmployeeGroupController(
-    private val employeeGroupService: EmployeeGroupService) {
+    private val employeeGroupService: EmployeeGroupService
+) {
 
     @GetMapping
     @PreAuthorize("hasAnyAuthority(T(com.sc.clinic.service.model.AuthRole).BASIC_USER)")
@@ -25,7 +20,6 @@ class EmployeeGroupController(
     @GetMapping("/count")
     @PreAuthorize("hasAnyAuthority(T(com.sc.clinic.service.model.AuthRole).BASIC_USER)")
     fun countGroups(@PathVariable companyId: Long) = employeeGroupService.countGroups(companyId)
-
 
 
 }
--- FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/dto/EmployeeGroupDto.kt
+++ FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/dto/EmployeeGroupDto.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class EmployeeGroupDto(
     @param:JsonProperty("id")
     val id: Long?,
-    @param:JsonProperty("companyName")
+    @param:JsonProperty("groupName")
     val groupName: String,
     @param:JsonProperty("employeeTypes")
     val employeeTypes: List<EmployeeTypeDto> = mutableListOf()
