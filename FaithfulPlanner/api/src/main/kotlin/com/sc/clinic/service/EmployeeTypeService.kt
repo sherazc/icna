7e3577d22e50f81a872a0d8af9b1290ad88d4a4b
sheraz@mbp16 <sheraz@bitsegment.com>
2026-02-20 07:44:19 -0500

    Save Employee

--- FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/service/EmployeeTypeService.kt
+++ FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/service/EmployeeTypeService.kt
@@ -1,7 +1,6 @@
 package com.sc.clinic.service
 
 import com.sc.clinic.dto.EmployeeTypeDto
-import com.sc.clinic.entity.EmployeeType
 import com.sc.clinic.entity.UserProfile
 import com.sc.clinic.repository.EmployeeTypeRepository
 import org.springframework.stereotype.Service
@@ -9,13 +8,17 @@ import org.springframework.stereotype.Service
 @Service
 class EmployeeTypeService(private val employeeTypeRepository: EmployeeTypeRepository) {
 
-    fun findByEmployeeGroupId(employeeGroupId: Long) = employeeTypeRepository.findByEmployeeGroupIdOrderByTypeName(employeeGroupId)
+    fun findByEmployeeGroupId(employeeGroupId: Long) =
+        employeeTypeRepository.findByEmployeeGroupIdOrderByTypeName(employeeGroupId)
 
-    fun findDtoByEmployeeGroupId(employeeGroupId: Long) = findByEmployeeGroupId(employeeGroupId).map { EmployeeTypeDto(it) }
+    fun findDtoByEmployeeGroupId(employeeGroupId: Long) =
+        findByEmployeeGroupId(employeeGroupId).map { EmployeeTypeDto(it) }
 
     fun updateEmployeeTypes(userProfileEntity: UserProfile, userEmployeeTypes: List<EmployeeTypeDto>) {
-        val employeeTypeIds = userEmployeeTypes.map { it.id }
-        val employeeTypes =  employeeTypeRepository.findAllById(employeeTypeIds).toMutableSet()
+        val employeeTypeIds = userEmployeeTypes
+            .filter { it.id != null }
+            .map { it.id!! }
+        val employeeTypes = employeeTypeRepository.findAllById(employeeTypeIds).toMutableSet()
         userProfileEntity.employeeTypes = employeeTypes
     }
 }
