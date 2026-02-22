74cdb4ad1586cbd9fdd33f207b5e16df451ba108
sheraz@mbp16 <sheraz@bitsegment.com>
2026-02-22 08:46:54 -0500

    Employee Group

--- FaithfulPlanner/ui/src/components/Settings/EmployeeGroupSettings.tsx
+++ FaithfulPlanner/ui/src/components/Settings/EmployeeGroupSettings.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import type { EmployeeGroupTypesDto } from "../../service/service-types";
 import { AppContext } from "../../store/context";
+import "EmployeeGroupSettings.css";
 
 interface Props { }
 export const EmployeeGroupSettings: React.FC<Props> = () => {
@@ -23,29 +24,12 @@ export const EmployeeGroupSettings: React.FC<Props> = () => {
   return (
     <div className="card">
       <h3>Employee Group</h3>
-      <div>
-        <div>
-          <label >Provider Group</label>
-          <input type="text" />
-          <button className="actionBtn actionBtnEdit" >Add Group</button>
-        </div>
-
-        {/* 
-        <div className="formGroup">
-          <label >Email</label>
-          <input type="text" />
-          <button className="actionBtn actionBtnEdit" >Add Group</button>
-        </div>
-        */}
-
-      </div>
       {groups && groups.length > 0 && groups.map(group => (
         <div key={group.id}>
           <h4>{group.groupName}</h4>
           {group.employeeTypes && group.employeeTypes.length > 0 && group.employeeTypes.map(employeeType => (
             <div key={employeeType.id}>
               <div>{employeeType.typeName}</div>
-
             </div>
           ))}
         </div>
