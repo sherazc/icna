61d02eb2f87cd16ef097b457aa936815acbfa110
sheraz@mbp16 <sheraz@bitsegment.com>
2026-03-08 18:03:37 -0400

    Delete employee

--- FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/entity/UserProfile.kt
+++ FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/entity/UserProfile.kt
@@ -32,7 +32,7 @@ data class UserProfile(
     @JoinColumn(name = "employee_group_id")
     var employeeGroup: EmployeeGroup? = null,
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
     @JoinTable(
         name = "m2m_user_profile_user_role",
         joinColumns = [JoinColumn(name = "user_profile_id")],
@@ -40,7 +40,7 @@ data class UserProfile(
     )
     var userRoles: MutableSet<UserRole>? = mutableSetOf(),
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
     @JoinTable(
         name = "m2m_user_profile_employee_type",
         joinColumns = [JoinColumn(name = "user_profile_id")],
