5be6597dc0410a7b2da1fb2630dbfa614005275f
sheraz@mbp16 <sheraz@bitsegment.com>
2026-03-21 11:32:17 -0400

    Operation day - Dashboard

--- FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/repository/OperationDaySpecification.kt
+++ FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/repository/OperationDaySpecification.kt
@@ -1,10 +1,20 @@
 package com.sc.clinic.repository
 
+import com.sc.clinic.entity.Company
 import com.sc.clinic.entity.OperationDay
+import jakarta.persistence.criteria.Join
+import jakarta.persistence.criteria.JoinType
 import org.springframework.data.jpa.domain.Specification
 import java.time.LocalDate
 
 object OperationDaySpecification {
+
+    fun ofCompany(companyId: Long): Specification<OperationDay> = {
+        root, query, cb ->
+            // root.fetch<OperationDay, Company>("company", JoinType.LEFT)
+            val companyJoin = root.join<OperationDay, Company>("", JoinType.LEFT)
+            cb.equal(companyJoin.get<Int>("company.id"), companyId)
+    }
 
     fun after(date: LocalDate?): Specification<OperationDay> = { root, _, cb ->
         date?.let { ld ->
