3e0450d034d9e4b6a4d0c1bd6812b6540d7ab034
sheraz@mbp16 <sheraz@bitsegment.com>
2026-06-02 19:07:22 -0400

    Enabled test URL to enable IP address access.

--- FaithfulPlanner/ui/vite.config.ts
+++ FaithfulPlanner/ui/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // below configuration is making local IP addresses work
+  // e.g. http://127.0.0.1:5173/, http://10.0.0.180:5173/
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+  },
+});
