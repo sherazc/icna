f239dea5ea08e8fa2d6a82c0d4a4b1c3f4c2ce88
sheraz@mbp16 <sheraz@bitsegment.com>
2026-07-20 00:11:59 -0400

    Operation day's Teams

--- FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/service/TeamService.kt
+++ FaithfulPlanner/api/src/main/kotlin/com/sc/clinic/service/TeamService.kt
@@ -1,6 +1,7 @@
 package com.sc.clinic.service
 
 import com.sc.clinic.dto.TeamDto
+import com.sc.clinic.entity.Company
 import com.sc.clinic.entity.Team
 import com.sc.clinic.repository.TeamRepository
 import org.springframework.stereotype.Service
@@ -16,8 +17,6 @@ class TeamService(
     fun getAllTeamDtoList(companyId: Long): List<TeamDto> = getAllTeams(companyId).map { TeamDto(it) }
 
     fun saveTeam(companyId: Long, teamDtoList: List<TeamDto>): TeamDto {
-
-        val company = companyService.findById(companyId)
         val existingTeams = teamRepository.findByCompanyId(companyId)
 
         // Delete Team - That user deleted
@@ -31,14 +30,30 @@ class TeamService(
 
         // Delete Team Employee Type - That user deleted
         existingTeams.forEach { team ->
-            team.employeeTypes.forEach { et -> teamEmployeeTypeService.deleteIfNotExists(team.id, et.id, teamDtoList)}
+            team.employeeTypes.forEach { et ->
+                teamEmployeeTypeService.deleteIfNotExists(team.id, et.id, teamDtoList)}
+        }
+
+        // Save Team and its employee types
+        val company = companyService.findById(companyId)
+        teamDtoList.map { teamDto ->
+            val team:Team = getOrCreateTeam(teamDto, company)
+            teamRepository.save(team)
+
         }
 
 
         TODO("Not yet implemented - Make it like EmployeeGroupService.save()")
     }
 
-
+    private fun getOrCreateTeam(teamDto: TeamDto, company: Company): Team {
+        return if ((teamDto.id ?: 0) > 0) {
+            teamRepository.findById(teamDto.id)
+                .orElse(Team(null, company, teamDto.teamName, mutableSetOf()))
+        } else {
+            Team(null, company, teamDto.teamName, mutableSetOf())
+        }
+    }
 
 
 }
