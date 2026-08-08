83cfcb34cc042c4134f3118dd040e03771f7074a
sheraz@mbp16 <sheraz@bitsegment.com>
2026-08-08 10:19:19 -0400

    Teams

--- FaithfulPlanner/ui/src/components/Settings/TeamSettings.tsx
+++ FaithfulPlanner/ui/src/components/Settings/TeamSettings.tsx
@@ -1,13 +1,16 @@
 import { useContext, useEffect, useState } from "react";
-import { defaultEmployeeGroupTypeDto, defaultTeamDto, FormState, ModalType, type EmployeeGroupTypesDto, type EmployeeTypeDto, type ErrorDto, type ModalConfig, type SelectOption, type TeamDto } from "../../service/service-types";
+import {
+  defaultTeamDto,
+  FormState,
+  type EmployeeGroupTypesDto,
+  type ErrorDto,
+  type SelectOption,
+  type TeamDto
+} from "../../service/service-types";
 import { AppContext } from "../../store/context";
 import "./TeamSettings.css";
-import { Modal } from "../common/Modal";
-import { touchNumber, touchString } from "../../service/utilities";
-import { toScErrorResponses, validateEmployeeGroupsForm } from "../../service/errors-helpers";
+import { touchString } from "../../service/utilities";
 import { ErrorForm } from "../common/ErrorForm";
-import { Loading } from "../common/Loading";
-import { ActionNameEmployeeGroup } from "../../store/employeeGroupsReducer";
 
 interface Props { }
 
@@ -29,6 +32,7 @@ export const TeamSettings: React.FC<Props> = () => {
     setAllGroupTypes(gTypes);
   };
 
+  // will be used to create drop down of employee type
   const createEmployeeTypeOptions = (gTypes: EmployeeGroupTypesDto[]): SelectOption[] => {
     const options: SelectOption[] = []
     gTypes.forEach(gType => {
@@ -59,60 +63,10 @@ export const TeamSettings: React.FC<Props> = () => {
     setTeams(allTeams);
   };
 
-  const createGroupCard = (group: EmployeeGroupTypesDto) => (
-    <div key={group.id} className="group-card">
-      <div className="group-header">
-        <div className="group-title">
-          <input
-            type="text"
-            defaultValue={group.groupName}
-            className={group.groupName ? "group-name-input" : "group-name-input input-empty"}
-            placeholder="Group name"
-          // onChange={(e) => onChangeGroupName(e, touchNumber(group.id))}
-          />
-        </div>
-        <div className="group-actions">
-          <button className="btn btn-icon btn-edit" title="Edit group">✎</button>
-          <button className="btn btn-icon btn-delete" title="Delete group"
-          // onClick={() => onDeleteGroup(authUserToken.companyId, touchNumber(group.id))}
-          >🗑</button>
-        </div>
-      </div>
-      <div className="employee-types-section">
-        <div className="section-label">Employee Types</div>
-        {group.employeeTypes && group.employeeTypes.length > 0 ? (
-          <div className="employee-types-list">
-            {group.employeeTypes.map(employeeType => createEmployeeTypeField(touchNumber(group.id), employeeType))}
-          </div>
-        ) : (
-          <div className="empty-state">No employee types added</div>
-        )}
-        <button className="btn btn-secondary btn-sm btn-add-type"
-        // onClick={() => onAddType(touchNumber(group.id))}
-        >+ Add Type</button>
-      </div>
-    </div>
-  );
-
-  const createEmployeeTypeField = (groupId: number, employeeType: EmployeeTypeDto) => (
-    <div key={employeeType.id}
-      className={employeeType.typeName ? "employee-type-item" : "employee-type-item input-empty"}>
-      <input
-        type="text"
-        defaultValue={employeeType.typeName}
-        className="employee-type-input"
-        placeholder="Employee type name"
-      // onChange={e => onChangeTypeName(e, groupId, touchNumber(employeeType.id))}
-      />
-      <button className="btn btn-icon btn-remove" title="Remove type"
-      // onClick={() => onDeleteType(groupId, touchNumber(employeeType.id))}
-      >×</button>
-    </div>
-  );
 
   return (
     <>
-      <div className="card employee-group-settings">
+      <div className="card team-settings">
         <div className="settings-header">
           <h2>Teams</h2>
           <button className="btn btnPrimary btn-sm" onClick={onAddTeam}>+ Add Team</button>
@@ -123,16 +77,7 @@ export const TeamSettings: React.FC<Props> = () => {
         {/* <Loading formState={formState} /> */}
 
         <div className="groups-container">
-          {/*         
-          {groups && groups.length > 0 ? (
-            groups.map(group => createGroupCard(group))
-          ) : (
-            <div className="empty-state-container">
-              <p>No employee groups created yet</p>
-              <button className="btn btnPrimary" onClick={onAddGroup}>Create First Group</button>
-            </div>
-          )}
-           */}
+      
         </div>
         {/* <Loading formState={formState}/> */}
         <div className="settings-footer">
@@ -141,10 +86,7 @@ export const TeamSettings: React.FC<Props> = () => {
           >Save All Teams</button>
         </div>
       </div>
-      {/* 
-      <Modal setShow={setModalShow} show={modalShow} config={modalConfig}>
-        {modalMessage}
-      </Modal> */}
+      
     </>
   );
 }
