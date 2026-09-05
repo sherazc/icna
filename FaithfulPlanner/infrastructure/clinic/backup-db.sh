7bfa1e844d541bd417d7747b783fc6d0536a2b0c
sheraz@mbp16 <sheraz@bitsegment.com>
2026-09-05 09:35:38 -0400

    Backup

---
+++ FaithfulPlanner/infrastructure/clinic/backup-db.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# set
+# -e = script will fail on error and will not continue
+# -o = Turn on the option coming next
+# pipefail = Fail any on any pipeline command. This script uses pipe "|"". Look at docker ... | gzip ...
+
+set -e -o pipefail
+
+# "$(dirname "$0")/ = script's current directory will be where the script is
+source "$(dirname "$0")/.env"
+
+mkdir -p "$backup_dir"
+backup_file="$backup_dir/${db_name}-$(date +%Y%m%d-%H%M%S).sql.gz"
+
+docker exec -e PGPASSWORD="$db_password" "$db_name" \
+  pg_dump -U "$db_user" -d "$db_name" | gzip > "$backup_file"
+
+echo "Wrote $backup_file"
+
+# Restore:
+#   gunzip -c backup_file | docker exec -i clinic_db psql -U clinic_user -d clinic_db
+
+
+# Hardcoded export command
+# Export to SQL
+docker exec -e PGPASSWORD="password" "clinic_db" \
+   pg_dump -U "clinic_user" -d "clinic_db" --insert > clinic_db_backup.sql
+
+# Export to zip
+docker exec -e PGPASSWORD="password" "clinic_db" \
+   pg_dump -U "clinic_user" -d "clinic_db" --insert | gzip > clinic_db_backup.sql.gz
