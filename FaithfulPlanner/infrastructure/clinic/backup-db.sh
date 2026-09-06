fed21728acaa694220ed793386490d7ffa4d3175
sheraz@mbp16 <sheraz@bitsegment.com>
2026-09-06 10:16:59 -0400

    Backup

--- FaithfulPlanner/infrastructure/clinic/backup-db.sh
+++ FaithfulPlanner/infrastructure/clinic/backup-db.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#
+# 
 # set
 # -e = script will fail on error and will not continue
 # -o = Turn on the option coming next
@@ -8,7 +8,10 @@
 set -e -o pipefail
 
 # "$(dirname "$0")/ = script's current directory will be where the script is
-source "$(dirname "$0")/.env"
+# cd into the script's own dir so .env's `app_dir=$(pwd)` resolves correctly
+# even when launched from cron (where pwd is $HOME, not this folder)
+cd "$(dirname "$0")"
+source .env
 
 mkdir -p "$backup_dir"
 backup_file="$backup_dir/${db_name}-$(date +%Y%m%d-%H%M%S).sql.gz"
@@ -18,26 +21,27 @@ docker exec -e PGPASSWORD="$db_password" "$db_name" \
 
 echo "Wrote $backup_file"
 
-# Restore:
-#   gunzip -c backup_file | docker exec -i clinic_db psql -U clinic_user -d clinic_db
-
-
 # Hardcoded export command
-# Export to SQL
-docker exec -e PGPASSWORD="password" "clinic_db" \
-   pg_dump -U "clinic_user" -d "clinic_db" \
-   --insert --clean --if-exists > clinic_db_backup.sql
+# SQL export
+# docker exec -e PGPASSWORD="password" "clinic_db" \
+#    pg_dump -U "clinic_user" -d "clinic_db" \
+#    --insert --clean --if-exists > clinic_db_backup.sql
 
-# Export to zip
-docker exec -e PGPASSWORD="password" "clinic_db" \
-   pg_dump -U "clinic_user" -d "clinic_db" \
-   --insert --clean --if-exists | gzip > clinic_db_backup.sql.gz
+# ZIP export
+# docker exec -e PGPASSWORD="password" "clinic_db" \
+#    pg_dump -U "clinic_user" -d "clinic_db" \
+#    --insert --clean --if-exists | gzip > clinic_db_backup.sql.gz
 
 # Hardcoded import command
-docker exec -i -e PGPASSWORD="password" "clinic_db" \
-   psql -U "clinic_user" -d "clinic_db" < clinic_db_backup.sql
+# SQL import
+# docker exec -i -e PGPASSWORD="password" "clinic_db" \
+#    psql -U "clinic_user" -d "clinic_db" < clinic_db_backup.sql
 
 
+# ZIP import
+# gunzip -c clinic_db_backup.sql.gz | docker \
+#    exec -i clinic_db psql -U clinic_user -d clinic_db
 
-scp -r -i ~/.ssh/id_rsa \
-  sheraz@10.0.0.20:clinic_db_backup.sql.gz .
+# Hardcoded copy command
+# scp -r -i ~/.ssh/id_rsa \
+#   sheraz@10.0.0.20:clinic_db_backup.sql.gz .
