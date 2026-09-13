692a369bd0343fb4fc49f22682d40d4ae1a80f43
sheraz@mbp16 <sheraz@bitsegment.com>
2026-09-13 18:38:53 -0400

    App routes

--- FaithfulPlanner/ui/src/routes/AppRoutes.tsx
+++ FaithfulPlanner/ui/src/routes/AppRoutes.tsx
@@ -16,18 +16,26 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Layout01/>}>
+        {/* Unauthenticated views */}
         <Route index element={<Login/>}/>
-        <Route path="login" element={<Login/>}/>
-        <Route path="dashboard" element={<Dashboard/>}/>
+        <Route path="/company-registration" element={<CompanyRegistration/>}/>
+        <Route path="/login" element={<Login/>}/>
+        
+
+        {/* Not used pages. Copy paste template pages from UI design */}
+        <Route path="/org-selection" element={<OrgSelection/>}/>
+        <Route path="/org-management" element={<OrgManagement/>}/>
+        <Route path="/shift-details" element={<ShiftDetails/>}/>
+        <Route path="/schedules" element={<Schedules/>}/>
+        <Route path="/notifications" element={<Notifications/>}/>
+        <Route path="/volunteer-reports" element={<VolunteerReports/>}/>
+
+
+        {/* Authenticated views */}
+        <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/employee-group/:employeeGroupId" element={<EmployeeGroup/>}/>
-        <Route path="company-registration" element={<CompanyRegistration/>}/>
-        <Route path="org-selection" element={<OrgSelection/>}/>
-        <Route path="org-management" element={<OrgManagement/>}/>
-        <Route path="schedules" element={<Schedules/>}/>
-        <Route path="shift-details" element={<ShiftDetails/>}/>
-        <Route path="notifications" element={<Notifications/>}/>
-        <Route path="settings" element={<Settings/>}/>
-        <Route path="volunteer-reports" element={<VolunteerReports/>}/>
+        <Route path="/settings" element={<Settings/>}/>
+
       </Route>
     </Routes>
   );
