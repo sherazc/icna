fae4825cbe3a55329ee34b59db4598cebf32a451
sheraz@mbp16 <sheraz@bitsegment.com>
2026-09-20 07:38:40 -0400

    Company slug name

--- FaithfulPlanner/ui/src/components/auth/UnAuthRedirect.tsx
+++ FaithfulPlanner/ui/src/components/auth/UnAuthRedirect.tsx
@@ -1,5 +1,5 @@
 import { type FC, useContext } from 'react';
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { AppContext } from "../../store/context";
 import type { AuthRole } from '../../service/service-types';
 import { isAuthenticated } from '../../service/authentication-services';
@@ -13,7 +13,12 @@ interface Props {
 
 export const UnAuthRedirect: FC<Props> = ({authenticated, shouldHaveRoles, shouldHaveAnyRoles }) => {
     const [{ authUserToken, companySlugName }] = useContext(AppContext);
+    const location = useLocation();
     const showContent = isAuthenticated(authenticated, authUserToken, shouldHaveRoles, shouldHaveAnyRoles);
+
+    const relativeUrl = location.pathname + location.search;
+    console.log(relativeUrl);
+
     return (
         <>
             {!showContent && <Navigate to={prefixCompanySlugNameUrl(companySlugName, "/login")} />}
--- FaithfulPlanner/ui/src/hook/useCompanySlugName.ts
+++ FaithfulPlanner/ui/src/hook/useCompanySlugName.ts
@@ -13,21 +13,9 @@ export const useCompanySlugName = () => {
   const navigate = useNavigate();
   const [{ clinicApis, authUserToken, companySlugName }, dispatch] = useContext(AppContext);
 
-
   const loadData = async () => {
-    // Create index Component. Route it to /. Check slug and auth and redirect to dashboard or login
-
-    // Do above or below
-
-    // Check slug and auth and redirect to dashboard or login
-
-
-    // Check if slugCompany is already loaded. if true then do not load it
-    // Check if slugCompany is different. If true then logout and redirect to /:companySlugName/login
-    // analysis if /login should redirect to /dashboard
     // analysis how to redirect to auth page after successful login. Currently it is being done in <UnAuthRedirect/>
     if (companySlugNameUrl && companySlugNameUrl.length > 0) {
-
       try {
         const company = await clinicApis.getCompanyBySlug(companySlugNameUrl);
         dispatch({ type: ActionNameCompanySlugName.companySlugNameUrlSet, payload: company });
@@ -37,53 +25,31 @@ export const useCompanySlugName = () => {
     }
   };
 
+  const navigateIfNeeded = async () => {
+    let companySlugNameLoaded = companySlugName;
+    if ((!companySlugNameLoaded || !companySlugNameLoaded.id) && isAuthenticated(true, authUserToken)) {
+      companySlugNameLoaded = await clinicApis.getCompanyById(authUserToken.companyId);
+      dispatch({ type: ActionNameCompanySlugName.companySlugNameUrlSet, payload: companySlugNameLoaded })
+    }
+
+    if (companySlugNameUrl && companySlugNameUrl.length > 0 && companySlugNameLoaded.slugName.length > 0 && companySlugNameLoaded.slugName !== companySlugNameUrl) {
+      dispatch({ type: ActionNameAuthUser.authUserLogout });
+      dispatch({ type: ActionNameCompanySlugName.companySlugNameUrlRemove });
+      navigate(prefixCompanySlugNameUrl(companySlugNameLoaded, "/login"));
+    }
+
+    // console.log("companySlugNameUrl", companySlugNameUrl);
+    // console.log("location", location);
+    // console.log("auToken", authUserToken);
+    // console.log("companySlugName", companySlugNameLoaded);
+    // console.log("=============");
+  };
+
   useEffect(() => {
     loadData()
   }, [companySlugNameUrl]);
 
   useEffect(() => {
-    const navigateIfNeeded = async () => {
-      let companySlugNameLoaded = companySlugName;
-      if ((!companySlugNameLoaded || !companySlugNameLoaded.id) && isAuthenticated(true, authUserToken)) {
-        companySlugNameLoaded = await clinicApis.getCompanyById(authUserToken.companyId);
-        dispatch({ type: ActionNameCompanySlugName.companySlugNameUrlSet, payload: companySlugNameLoaded })
-      }
-
-      if (companySlugNameUrl && companySlugNameUrl.length > 0 && companySlugNameLoaded.slugName.length > 0 && companySlugNameLoaded.slugName !== companySlugNameUrl) {
-        dispatch({ type: ActionNameAuthUser.authUserLogout });
-        dispatch({ type: ActionNameCompanySlugName.companySlugNameUrlRemove });
-        navigate(prefixCompanySlugNameUrl(companySlugNameLoaded, "/login"));
-      }
-
-      // console.log("companySlugNameUrl", companySlugNameUrl);
-      // console.log("location", location);
-      // console.log("auToken", authUserToken);
-      // console.log("companySlugName", companySlugNameLoaded);
-      // console.log("=============");
-
-
-      // if (isAuthenticated(true, authUserToken)
-      //     && companySlugNameLoaded.id && companySlugNameLoaded.id > 0
-      //     && companySlugNameLoaded.id !== authUserToken.companyId
-      //     && companySlugNameLoaded.slugName) {
-
-      // dispatch({ type: ActionNameAuthUser.authUserLogout });
-      // navigate(`${companySlugNameLoaded.slugName}/login`)
-      // } 
-
-      // else if (location.pathname && location.pathname === "/" && companySlugNameLoaded.id && companySlugNameLoaded.id > 0) {
-      //   if (isAuthenticated(true, authUserToken)) {
-      //     navigate(`${companySlugNameLoaded.slugName}/dashboard`)
-      //   } else {
-      //     navigate(`${companySlugNameLoaded.slugName}/login`)
-      //   }
-
-      // }
-
-
-    };
-
     navigateIfNeeded();
-
   }, [location, authUserToken, companySlugName, companySlugNameUrl]);
 };
